3008cea6b0e8e2c6f3b89b1e625286de13561c7e
Svitlana Filatova <svitlana.filatova.gmail.com>
2019-04-05 15:58:03 -0700

    It will have known bugs in README

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
 // Business logic
-words = ["Beep!", "Boop!", "I'm sorry, Dave. I'm afraid I can't do that"]
+word1 = ["Beep!"]
+word2 = ["Boop!"]
+word3 = ["I'm sorry, Dave. I'm afraid I can't do that"]
+
 var hasDigit = function(number, digit) {
   return number.toString().includes(digit.toString())
-}
+  }
 
+var rangeOfnumbers = function(number) {
+  var numbers = []
+  var number = parseInt($("input#userInput").val());
+  for (var i = 0; i <= number; i = i + 1) {
+    if (hasDigit(i, 3)) {
+      numbers.push(word3)
+    } else if (hasDigit(i, 2)) {
+      numbers.push(word2)
+    } else if (hasDigit(i, 1)) {
+      numbers.push(word1)
+    } else {
+      numbers.push(i)
+    }
+  }
+  return numbers.join(", ")
+}
 
 // UI logic
 $(document).ready(function() {
@@ -13,23 +32,23 @@ $(document).ready(function() {
     if (($("input#userInput").val() === "")) {
       alert("Please enter a number");
     } else {
-      var numbers = []
-      for (var i = 0; i<=number; i = i + 1) {
-        if (hasDigit(i, 3)) {
-          numbers.push(words[2])
-        } else if (hasDigit(i, 2)) {
-          numbers.push(words[1])
-        } else if (hasDigit(i, 1)) {
-          numbers.push(words[0])
-        } else {
-          numbers.push(i)
-        }
-      }
+      var modifiedrRange = rangeOfnumbers(number)
+      console.log(modifiedrRange)
+      $(".output").text(modifiedrRange).show();
     }
-    console.log(numbers)
-
-
-    // $("#result").text(output);
     // $("#result").show();
   });
 });
+
+// var numbers = []
+// for (var i = 0; i<=number; i = i + 1) {
+//   if (hasDigit(i, 3)) {
+//     numbers.push(words[2])
+//   } else if (hasDigit(i, 2)) {
+//     numbers.push(words[1])
+//   } else if (hasDigit(i, 1)) {
+//     numbers.push(words[0])
+//   } else {
+//     numbers.push(i)
+//   }
+// }
